6316661998fdd76b0c47d6fbaf4c6c0fbc9af026
ilaicraftYT <70978042+ilaicraftYT@users.noreply.github.com>
2021-09-16 16:10:27 -0500

    Added something

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # AntiForceOP
-Protect your server from intruders with OP!
+Not all players are nice and good, some just want your server be destroyed, for that we are here.<br>
+Protect your server from being OP forced and/or intruders in your console giving OP to everyone.<br>
+
+- [x] Easy to use
+- [x] Simple and fast
+- [x] 100% Customizable
+- [x] Open source
+
+## TODO
+
+- MySQL/SQLITE Support
+
+If you have a good idea, see the contribute tab!
+
+## Contributing
+
+Issues and Pull requests are welcome! If you want a new feature, don't leave it in the reviews, leave it [here](https://github.com/SSHStudios/issues).
--- src/ilai/AntiForceOP/Main.php
+++ src/ilai/AntiForceOP/Main.php
@@ -11,6 +11,8 @@ use pocektmine\utils\Config;
 use pocketmine\utils\TextFormat as C;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 
 class Main extends PluginBase implements Listener {
 
@@ -18,13 +20,57 @@ class Main extends PluginBase implements Listener {
         $this->getServer()->getPluginManager()->registerEvents($this,  $this);
         $this->saveDefaultConfig();
         $this->reloadConfig();
-        $this->getLogger()->info("AntiForceOP loaded.");
-        if($this->getConfig()->get("disableInitialWarning") !== true) {
+        $this->getLogger()->info("AntiForceOP plugin loaded.");
+        if($this->getConfig()->get("disableInitialWarning") !== true && $this->getConfig()->get("enable") == true) {
             $this->getLogger()->warning("Every player who haves OP and hasn't been registered on the configuration file will be kicked!");
         }
     }
-
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+        // The player cannot execute comamnd if isn't registered:
+        if($this->getConfig->get("enable")) {
+            $player = $sender->getPlayer();
+            $playerName = $player->getName();
+            $allowedPlayers = $this->getConfig()->get("allowed", []);
+            if(!in_array($playerName, $allowedPlayers)){
+            $ev->setKickMessage($this->getConfig()->get("kickMessage"));
+            $ev->setCancelled(true);
+            $this->getLogger()->notice("AntiForceOP has detected a unregistered OP: " . $playerName . "!");
+          }
+        }
+        // Now, the commands of the plugin
+        switch (strtolower($command->getName())) {
+            case 'forceop':
+                if(!$sender instanceof Player){
+                    $sender->sendMessage("You cannot disable protection as console.");
+                    return false;
+                } else {
+                    if(!isset($args[0])){
+                        $sender->sendMessage(C::RED . "Type: /forceop disable/enable");
+                    } else {
+                        switch (strtolower($args[0])) {
+                            case 'disable':
+                                if ($sender->hasPermission("antiforceop.commands.manage")) {
+                                    $sender->sendMessage(C::GREEN . "Anti-ForceOP protection has been disabled.");
+                                } else {
+                                    $sender->sendMessage(C::RED . "You don't have the required permissions.");
+                                    return false;
+                                }
+                                break;
+                            case "enable":
+                            if ($sender->hasPermission("antiforceop.commands.manage")) {
+                                $sender->sendMessage(C::GREEN . "Anti-ForceOP protection has been enabled.");
+                            } else {
+                                $sender->sendMessage(C::RED . "You don't have the required permissions");
+                            }
+                                break;
+                        }
+                    }
+                }
+                break;
+        }
+    }
     public function onLogin(PlayerLoginEvent $ev){
+        if(!$this->getConfig->get("enable")) return;
         $player = $ev->getPlayer();
         $playerName = $player->getName();
         $allowedPlayers = $this->getConfig()->get("allowed", []);
